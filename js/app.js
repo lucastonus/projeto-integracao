8df32294d1a4d28859b1ad45812c61e667281668
Lucas <Lucas@LUCAST-PC>
2017-06-24 15:18:58 -0300

    Adição dos arquivos para o estilo da página.
    Sistema de busca.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+$(document).ready(function() {
+	vincularEventos();
+});
+
+function ajustarAlturaMapa() {
+	var windowHeight = $(window).height();
+	var mapHeight = windowHeight - 124;
+	$('#map').css('height', mapHeight+'px');
+}
+
+function vincularEventos() {
+	$('.search-button').on('click', function(){
+		ajustarAlturaMapa();
+		$('.descricao-busca').remove();
+		var repository = $('#repository-address').val();
+
+		App.map = new GMaps({
+			el: '#map',
+			lat: 0,
+			lng: 0,
+			zoom: 2
+		});
+
+		GitAPI.getRepo(repository);
+	});
+
+	$('.box-search h1').on('click', function() {
+		location.reload();
+	})
+}
+
 var App = {
 	map: null,
 
@@ -47,14 +78,3 @@ var GitAPI = {
 		return 'https://api.github.com' + path + '?client_id=867563d42e4c9d5fda03&client_secret=43f406c798f11c2b6c7dd77a041839d21c3a4db4';
 	}
 };
-
-$(function() {
-	App.map = new GMaps({
-		el: '#map',
-		lat: 0,
-		lng: 0,
-		zoom: 2
-	});
-
-	GitAPI.getRepo('lucastonus/projeto-integracao');
-});
