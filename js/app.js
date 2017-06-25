fe31e4a1edf7b8e89c0c2dcf4580288c73c22a4c
Adryan Signor <alexsandrosignor@hotmail.com>
2017-06-25 18:35:03 -0300

    Ajuste nas tags html e nos eventos de busca;

--- js/app.js
+++ js/app.js
@@ -1,34 +1,3 @@
-$(document).ready(function() {
-	vincularEventos();
-});
-
-function ajustarAlturaMapa() {
-	var windowHeight = $(window).height();
-	var mapHeight = windowHeight - 124;
-	$('#map').css('height', mapHeight+'px');
-}
-
-function vincularEventos() {
-	$('.search-button').on('click', function(){
-		ajustarAlturaMapa();
-		$('.descricao-busca').remove();
-		var repository = $('#repository-address').val();
-
-		App.map = new GMaps({
-			el: '#map',
-			lat: 0,
-			lng: 0,
-			zoom: 2
-		});
-
-		GitAPI.getRepo(repository);
-	});
-
-	$('.box-search h1').on('click', function() {
-		location.reload();
-	})
-}
-
 var App = {
 	map: null,
 
@@ -47,34 +16,64 @@ var App = {
 				callback: function(results, status) {
 					if (status == 'OK') {
 						var latlng = results[0].geometry.location;
-						var boxContent = '<p><img height="200" width="200" src="' + user.avatar_url + '"></img></p><p><b>' + user.name + '</b></p>';
-						App.map.addMarker({
-							lat: latlng.lat(),
-							lng: latlng.lng(),
-							icon: {
-								'url': user.avatar_url,
-								'anchor': new google.maps.Point(7.5,47),
-								'scaledSize': {'height': 15, 'width': 15}
-							},
-							infoWindow: {
-								content: boxContent
-							}
-						});
 
-						App.map.addMarker({
+						var marker = {
 							lat: latlng.lat(),
 							lng: latlng.lng(),
 							icon: {
-								'url': 'images/marker.png'
+								url: user.avatar_url,
+								anchor: new google.maps.Point(7.5, 47),
+								scaledSize: {height: 15, width: 15}
 							},
 							infoWindow: {
-								content: boxContent
+								content: $('<div>').append(
+									$('<b>').text(user.name),
+									$('<br/>'),
+									$('<img>', {height: 200, width: 200, src: user.avatar_url, alt: user.login})
+								).prop('outerHTML')
 							}
-						});
+						}
+
+						App.map.addMarker(marker);
+
+						marker.icon = {url: 'images/marker.png'};
+						App.map.addMarker(marker);
 					}
 				}
 			});
 		}
+	},
+
+	search: function() {
+		$('#map').css('height', $(window).height() - 124 + 'px');
+		$('.descricao-busca').remove();
+
+		App.map = new GMaps({
+			el: '#map',
+			lat: 0,
+			lng: 0,
+			zoom: 2
+		});
+
+		GitAPI.getRepo($('#repository-address').val().trim());
+	},
+
+	linkEvents: function() {
+		$('#repository-address').focus();
+
+		$('.search-button').on('click', function() {
+			App.search();
+		});
+
+		$('#repository-address').on('keypress', function(event) {
+			if (event.which == 13) {
+				App.search();
+			}
+		});
+
+		$('.box-search h1').on('click', function() {
+			window.open('https://github.com/lucastonus/projeto-integracao');
+		});
 	}
 };
 
@@ -89,3 +88,7 @@ var GitAPI = {
 		return 'https://api.github.com' + path + '?client_id=867563d42e4c9d5fda03&client_secret=43f406c798f11c2b6c7dd77a041839d21c3a4db4';
 	}
 };
+
+$(function() {
+	App.linkEvents();
+});
